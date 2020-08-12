567d2295b29311814c35a4d5b9b34e613aee6184
okitsu teppei <okitsu@H2020-289.local>
2020-08-12 16:25:52 +0900

    add project

---
+++ SwiftUITutorial/SwiftUITutorial/SwiftUITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITutorialApp.swift
+//  SwiftUITutorial
+//
+//  Created by okitsu teppei on 2020/08/12.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
